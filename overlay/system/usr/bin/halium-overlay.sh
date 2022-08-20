3baef16d8a4811f551df62d4044aced602376cae
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2022-08-21 02:02:06 +0300

    overlay: remove old vendor overlay script

--- overlay/system/usr/bin/halium-overlay.sh
+++
@@ -1,6 +0,0 @@
-HALIUM_OVERLAY="$(find /opt/halium-overlay/ -type f -print)"
-for overlay in $HALIUM_OVERLAY
-do
-    destination="$(echo $overlay | sed -e 's/\/opt\/halium-overlay\///')"
-    mount -o bind "$overlay" "/android/$destination"
-done
